42c10444caa1932307de09f48d9afea80ad7d7d5
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2023-12-20 13:01:00 -0500

    Responsive and Favicon.ico

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,44 @@
 export default function Home() {
   return (
     <div className="h-screen">
-      <div className=" p-3 flex flex-col gap-4X justify-center text-center h-full">
+      <div className=" p-3 flex flex-col text-center h-full">
         <span className=" pb-3 text-blue-600 text-xl font-extrabold">
           facebook
         </span>
 
-        <input
-          className=" mb-2 rounded-sm text-sm font-normal bg-gray-100 placeholder-gray-600"
-          type="text"
-          placeholder="Número de celular o correo electrónico"
-          name="email"
-        />
+        <div className=" flex flex-col w-7/12X w-full sm:w-96 self-center">
+          <input
+            className=" mb-2 rounded-sm text-sm font-normal bg-gray-100 placeholder-gray-600"
+            type="text"
+            placeholder="Número de celular o correo electrónico"
+            name="email"
+          />
 
-        <input
-          className=" mb-2 rounded-sm text-sm font-normal bg-gray-100 placeholder-gray-600"
-          type="text"
-          placeholder="Contraseña"
-          name="password"
-        />
+          <input
+            className=" mb-2 rounded-sm text-sm font-normal bg-gray-100 placeholder-gray-600"
+            type="text"
+            placeholder="Contraseña"
+            name="password"
+          />
 
-        <button className=" bg-blue-600 py-1.5 mb-3 font-semibold rounded-md text-white text-lg">
-          Iniciar sesión
-        </button>
+          <button className=" bg-blue-600 py-1.5 mb-3 font-semibold rounded-md text-white text-lg">
+            Iniciar sesión
+          </button>
 
-        <span className=" text-sm font-normal text-blue-600 mb-4">
-          ¿Olvidaste tu contraseaña?
-        </span>
+          <span className=" text-sm font-normal text-blue-600 mb-4">
+            ¿Olvidaste tu contraseaña?
+          </span>
 
-        <div className=" flex items-center gap-4 justify-center mb-3">
-          <hr className=" w-full border border-gray-200" />
-          <span className=" text-sm">o</span>
-          <hr className=" w-full border border-gray-200" />
+          <div className=" flex items-center gap-4 justify-center mb-3">
+            <hr className=" w-full border border-gray-200" />
+            <span className=" text-sm">o</span>
+            <hr className=" w-full border border-gray-200" />
+          </div>
+
+          <button className="mt-0.5 mb-3 border border-gray-400/80 w-4/5 self-center py-2 text-sm font-medium rounded">
+            Crear cuenta nueva
+          </button>
         </div>
-
-        <button className="mt-0.5 mb-3 border border-gray-400/80 w-4/5 self-center py-2 text-sm font-medium rounded">
-          Crear cuenta nueva
-        </button>
 
         <div className="bgX-yellow-400 flex-grow flex items-center text-xs text-gray-800">
           <div className="bgX-green-600 w-full flex flex-col">
